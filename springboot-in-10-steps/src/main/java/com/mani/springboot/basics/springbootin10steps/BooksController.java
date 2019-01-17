8b3a001d7581f21109b5528a1c7ab8fca19ee820
Manideep Vemula <manideepchandra@gmail.com>
2019-01-17 17:11:07 +0530

    Small App using Spring boot & its testing

---
+++ springboot-in-10-steps/src/main/java/com/mani/springboot/basics/springbootin10steps/BooksController.java
@@ -0,0 +1,19 @@
+package com.mani.springboot.basics.springbootin10steps;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BooksController {
+	
+	@GetMapping("/books")
+	public List<Book> getAllBooks()
+	{
+		return Arrays.asList(new Book(1l, "spring boot-2.0","Ranga karanam"));
+	}
+
+}
